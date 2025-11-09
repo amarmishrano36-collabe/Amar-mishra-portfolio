09eeeceb2cafe1708cb756e9a98a8441e1f684ca
amarmishrano36-collabe <amarmishrano.36@gmail.com>
2025-11-09 18:14:05 +0530

    second commit

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
- let text = document.querySelector(".text p");
-
-text.innerHTML = text.innerHTML.split("").map((char,i)=>
+let text = document.querySelector(".text p");
+text.innerHTML = text.innerHTML.split("").map((char, i) =>
     `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`
 ).join("");
 
-// scroll reveal
-
-ScrollReveal({ 
-    distance:"90px",
-    duration:2000,
-    delay:200,
+ScrollReveal({
+    distance: "100px",
+    duration: 4000,
+    delay: 400,
 });
-
-
-ScrollReveal().reveal('.hero-info,.main-text,.proposal,.heading', { origin: "top" });
-ScrollReveal().reveal('.about-img,.fillter-buttons,.contact-info', { origin: "left" });
-ScrollReveal().reveal('.about-content,.skills', { origin: "right" });
-ScrollReveal().reveal('.allServices,.portfolio-gallery,.blog-box,footer,.img-hero', { origin: "bottom" });
